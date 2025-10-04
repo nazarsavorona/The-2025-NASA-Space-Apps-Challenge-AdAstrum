d56b2b10d75b6380dd8964516f94b68b2c056ac2
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2025-10-04 16:56:17 +0300

    add preprocessing

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,17 @@
 import logging
+from io import BytesIO
+
 import aiofiles
+import pandas as pd
 from fastapi import APIRouter, FastAPI, UploadFile, HTTPException, status
 from pathlib import Path
 
+from model_api import call_model
+from preprocess import get_dataframe_format
+
 app = FastAPI()
+
+USER_FILE = "dynamic/user-data.csv"
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -14,10 +22,11 @@ router = APIRouter()
 def read_root():
     return {"message": "Hello, AdAstrum!"}
 
-@app.post("/start-session/")
-async def start_session(file: UploadFile | None):
-    filename = "dynamic/user-data.csv"
-    filepath = Path(filename)
+
+@app.post("/upload/")
+async def upload(file: UploadFile | None):
+
+    filepath = Path(USER_FILE)
     if file:
         try:
             async with aiofiles.open(filepath, "wb") as out_file:
@@ -46,3 +55,33 @@ async def start_session(file: UploadFile | None):
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail="Failed to remove session file"
             ) from exc
+
+@app.post("/predict/")
+async def get_result_for_file():
+    data_format = None
+    try:
+        df = pd.read_csv(USER_FILE)
+        data_format = get_dataframe_format(df)
+    except Exception as exs:
+        return HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=exs
+        )
+    return call_model(data_format, df)
+
+@app.post("/run-for-csv/")
+async def get_result_for_file(file: UploadFile | None):
+    if not file.filename.endswith(".csv"):
+        raise HTTPException(status_code=400, detail="Only CSV files are supported")
+    df = None
+    data_format = None
+    try:
+        contents = await file.read()
+        df = pd.read_csv(BytesIO(contents))
+        data_format = get_dataframe_format(df)
+    except Exception as exs:
+        return HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=exs
+        )
+    return call_model(data_format, df)
---
+++ backend/model_api.py
@@ -0,0 +1,15 @@
+
+import pandas
+import requests
+
+async def call_model(data_format: str, df: pandas.DataFrame):
+    url = "localhost:5000/predict"
+    payload = {
+        "format": data_format,
+        "content": df.to_json(orient="records")
+    }
+    response = requests.post(url, json=payload)
+    try:
+        return response.json()
+    except ValueError:
+        return response.text
