65c246792dec066cd1408b67f837d41c370c592b
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2025-10-05 13:55:48 +0300

    add real importance

--- src/app/graphs/page.js
+++ src/app/graphs/page.js
@@ -4,16 +4,10 @@
 import FeatureImportance from "@/app/graphs/FeatureImportance";
 
 export default function Graphs() {
-    const features = {
-        feature_a: 0.75,
-        feature_b: 0.15,
-        feacture_c: 0.1,
-    };
-
     return (
         <div>
             <div style={{ width: '100vw', height: '100vh' }}>
-                <FeatureImportance features={features} />
+                <FeatureImportance />
             </div>
         </div>
     );
